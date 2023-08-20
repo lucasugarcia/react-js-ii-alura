6ca495c74b048dd1a46e88cc4a26a40e78d78202
Lucas Garcia <lucasugarcia@gmail.com>
2023-08-20 14:41:28 -0300

    Versionamento inicial do projeto do segundo curso

---
+++ src/App.js
@@ -0,0 +1,60 @@
+import { useState } from 'react';
+import Banner from './components/Banner';
+import Formulario from './components/Formulario';
+import Time from './components/Time';
+import Rodape from './components/Rodape';
+
+function App() {
+  const times = [
+    {
+      nome: 'Administrativo',
+      corPrimaria: '#82cffa',
+      corSecundaria: '#e8f8ff'
+    },
+    {
+      nome: 'Desenvolvimento',
+      corPrimaria: '#57c278',
+      corSecundaria: '#d9f7e9'
+    },
+    {
+      nome: 'Manutenção',
+      corPrimaria: '#a6d157',
+      corSecundaria: '#f0f8e2'
+    },
+    {
+      nome: 'Processamento',
+      corPrimaria: '#e06b69',
+      corSecundaria: '#fde7e8'
+    },
+    {
+      nome: 'TI',
+      corPrimaria: '#feba05',
+      corSecundaria: '#fff5d9'
+    }
+  ]
+
+  const [colaboradores, setColaboradores] = useState([])
+
+  const aoNovoColaboradorAdicionado = (colaborador) => {
+    setColaboradores([...colaboradores, colaborador])
+  }
+
+  return (
+    <div className="App">
+      <Banner />
+      <Formulario times={times.map(time => time.nome)} aoColaboradorCadastrado={aoNovoColaboradorAdicionado}/>
+      
+      {times.map(time => <Time 
+        key={time.nome} 
+        nome={time.nome} 
+        corPrimaria={time.corPrimaria} 
+        corSecundaria={time.corSecundaria}
+        colaboradores={colaboradores.filter(colaborador => colaborador.time === time.nome)}
+      />)}
+
+      <Rodape />
+    </div>
+  );
+}
+
+export default App;
